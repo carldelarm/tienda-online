c134684a346fd83f21b120c5a7932a734afc3c29
INDRA\crosa <crosa@indracompany.com>
2024-04-20 09:51:56 -0500

    Se adiciona componentes card,chip,rating y se integra en el componente <Home>

--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -1,10 +1,21 @@
 //import CrButton from "../components/CrButton"
+import { Box } from "@mui/material"
+import CrCard from "../components/CrCard"
 import CrNavBar from "../components/CrNavBar"
 
 const Home = () => {
   return (
     <>
-      <CrNavBar />
+        <CrNavBar />
+        <Box sx={{
+            display: 'flex',
+            marginTop: 5,
+            gap: 2,
+        }}>
+            {[1,2,3].map((item) => (
+                <CrCard key={item}/>
+            ))}
+        </Box>
     </>
   )
 }
