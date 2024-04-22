7cdb14dce06da26973edbec61b74f356408a4751
INDRA\crosa <crosa@indracompany.com>
2024-04-22 05:03:05 -0500

    Se adicionan nuevos componentes de modal,icons,chip a la card

--- src/components/CrCard.tsx
+++ src/components/CrCard.tsx
@@ -2,33 +2,74 @@ import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import CrChip from './CrChip';
 import CrRating from './CrRating';
+import { Stack } from '@mui/material';
+import CrBtnAcion from './CrBtnAcion';
+import { useState } from 'react';
+import CrModal from './CrModal';
 
 export default function CrCard() {
+
+  const [isCheck, setCheckValue] = useState(true);
+  const [open, setOpen] = useState(false);
+
+  const handleCheck = () => {
+    setCheckValue(!isCheck);
+  }
+
+  const handleShow = () => {
+    console.log('handleShow')
+
+  }
+
+  //Relacionado con CrModal
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   return (
-    <Card sx={{ maxWidth: 345}}>
-      <CardMedia
-        sx={{ height: 140 }}
-        image="https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg"
-        title="green iguana"
-      />
-    <CardContent>
-        <CrChip />
-        <Typography gutterBottom variant="h5" component="div">
-          Lizard
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          Lizards are a widespread group of squamate reptiles, with over 6,000
-          species, ranging across all continents except Antarctica
-        </Typography>
-        <CrRating />
-      </CardContent>
-      <CardActions>
-        <Button size="small" variant='contained'>Agregar</Button>
-      </CardActions>
-    </Card>
+    <>
+      <Card sx={{ maxWidth: 345}}>
+        <Stack direction="row" m={2} spacing={2}>
+          <CrChip />
+        </Stack>
+        <CardMedia
+          sx={{ height: 500 }}
+          component={'img'}
+          image="https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg"
+          title="green iguana"
+          style={{
+            width: '200',
+            height: '200',
+            objectFit: 'contain' 
+          }}
+          alt='Producto'
+          />
+      <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            Lizard
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Lizards are a widespread group of squamate reptiles, with over 6,000
+            species, ranging across all continents except Antarctica
+          </Typography>
+          <CrRating />
+        </CardContent>
+        <CardActions sx={{
+            display: 'flex',
+            justifyContent: 'space-between'
+          }}>
+          <CrBtnAcion isShow isCheck={isCheck} handleShow={handleClickOpen} />
+          <CrBtnAcion isCheck={isCheck} handleCheck={handleCheck} />
+        </CardActions>
+      </Card>
+      <CrModal open={open} handleClose={handleClose} />
+    </>
   );
 }
